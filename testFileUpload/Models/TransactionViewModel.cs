c8c3bf454513aa6f622998aaba15bc63e4249010
Kaiwanyawit Chawankul <kaiwanyawit.c@draycir.com>
2019-12-23 22:28:01 +0700

    add transaction view model

---
+++ testFileUpload/Models/TransactionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testFileUpload.Models
+{
+    public class TransactionViewModel
+    {
+        public string Id { get; set; }
+        public string payment { get; set; }
+        public string Status { get; set; }
+    }
+}
