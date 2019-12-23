3dc290f7c08dae938340d486572eaa4c1d73383f
Kaiwanyawit Chawankul <kaiwanyawit.c@draycir.com>
2019-12-23 21:48:54 +0700

    add project

---
+++ testFileUpload/Controllers/FileController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using testFileUpload.Core.Data;
+using testFileUpload.Core.Models;
+using testFileUpload.Core.Services;
+
+namespace testFileUpload.Controllers
+{
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+        private readonly AppDbContext _context;
+        private readonly IFileService _fileService;
+        private readonly long _fileSizeLimit;
+
+
+        public FileController(ILogger<FileController> logger, AppDbContext context, IConfiguration config, IFileService fileService)
+        {
+            if (config is null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _fileService = fileService ?? throw new ArgumentNullException(nameof(fileService));
+            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
+
+            //// To save physical files to a path provided by configuration:
+            //_targetFilePath = config.GetValue<string>("StoredFilesPath");
+
+            // To save physical files to the temporary files folder, use:
+            //_targetFilePath = Path.GetTempPath();
+        }
+        public async Task<IActionResult> OnPostUploadAsync(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var filePath = Path.GetTempFileName();
+
+                    using (var stream = System.IO.File.Create(filePath))
+                    {
+                        //await formFile.CopyToAsync(stream);
+                        var importResult = await _fileService.Import(formFile.ContentType, stream);
+                        if (importResult.Status == ImportResultStatus.InvalidType)
+                        {
+                            return BadRequest("Unknown format");
+                        }
+                        else if (importResult.Status == ImportResultStatus.InvalidValidation)
+                        {                            
+                                return BadRequest();
+                        }
+                        else
+                        {
+                            return Ok();
+                        }
+                    }
+
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
