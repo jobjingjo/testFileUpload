74e4a5b9466b8286283055012feb0a7687c5fa11
Kaiwanyawit Chawankul <Kaiwanyawit.C@draycir.com>
2019-12-23 08:17:19 +0700

    starter

---
+++ testFileUpload.Core.Tests/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace testFileUpload.Core.Tests
+{
+    public class Class1
+    {
+    }
+}
---
+++ testFileUpload.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace testFileUpload.Core
+{
+    public class Class1
+    {
+    }
+}
