2831c9f26b64696e099dd002804aaa95321ff557
Kaiwanyawit Chawankul <Kaiwanyawit.C@draycir.com>
2019-12-24 09:12:56 +0700

    mock endpoint

---
+++ testFileUpload.Core/Data/TransactionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using testFileUpload.Core.Models;
+
+namespace testFileUpload.Core.Data
+{
+    public class TransactionsRepository : EfCoreRepository<Transaction, AppDbContext>
+    {
+        public TransactionsRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
--- testFileUpload.Core/Services/TransactionService.cs
+++ testFileUpload.Core/Services/TransactionService.cs
@@ -10,36 +10,52 @@ namespace testFileUpload.Core.Services
 {
     public class TransactionService : ITransactionService
     {
-        private readonly AppDbContext _context;
+        //private readonly AppDbContext _context;
 
-        public TransactionService(AppDbContext context) {
-            _context = context ?? throw new ArgumentNullException(nameof(context));
+        public TransactionService(
+            //AppDbContext context
+            ) {
+            //_context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
         public async Task<List<Transaction>> GetByCurrency(string currency)
         {
-            var transactions = await _context.Transactions.Where(t => t.CurrencyCode == currency)
-                .AsNoTracking()
-                .ToListAsync();
-            return transactions;
+
+            List<Transaction> transactions =new List<Transaction>();
+            transactions.Add(new Transaction()
+            {
+                Id = "inv-xxx-xx",
+                Amount = 1,
+                CurrencyCode = currency,
+                Status = TransactionStatus.Done,
+                TransactionDate = DateTime.Today
+            });
+            return await Task.FromResult(transactions);
+
+            //var transactions = await _context.Transactions.Where(t => t.CurrencyCode == currency)
+            //    .AsNoTracking()
+            //    .ToListAsync();
+            //return transactions;
         }
 
         public async Task<List<Transaction>> GetByDateRange(DateTime startDate, DateTime endDate)
         {
-            var transactions = await _context.Transactions
-                .Where(t => t.TransactionDate>=startDate)
-                .Where(t => t.TransactionDate <= endDate)
-                .AsNoTracking()
-                .ToListAsync();
-            return transactions;
+            //var transactions = await _context.Transactions
+            //    .Where(t => t.TransactionDate>=startDate)
+            //    .Where(t => t.TransactionDate <= endDate)
+            //    .AsNoTracking()
+            //    .ToListAsync();
+            //return transactions;
+            throw new NotImplementedException();
         }
 
         public async Task<List<Transaction>> GetByStatus(TransactionStatus status)
         {
-            var transactions = await _context.Transactions.Where(t => t.Status == status)
-               .AsNoTracking()
-               .ToListAsync();
-            return transactions;
+            //var transactions = await _context.Transactions.Where(t => t.Status == status)
+            //   .AsNoTracking()
+            //   .ToListAsync();
+            //return transactions;
+            throw new NotImplementedException();
         }
     }
 }
